b6ed9fb398c3a1bfc16324061ae5e2ce2b52ad51
Ezio Intravaia <ezio.intravaia@hotmail.com>
2020-12-05 17:38:48 +0000

    react installed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+import NavBar from "./components/NavBar"
+import Routes from "./containers/Routes"
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Routes = () => {
+    return (
+        <h1>routes works</h1>
+    )
+}
+
+export default Routes
