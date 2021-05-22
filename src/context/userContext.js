325df699272754e74b8b5d2aeeea38b812dd3e63
Ezio Intravaia <ezio.intravaia@hotmail.com>
2021-05-22 13:03:54 +0100

    fixed IP getter

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,97 +1,93 @@
 import { navigate } from "@reach/router";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import firebase, { firestore, provider } from "../firebase";
-import { CrudContext } from "./crudContext"
+import { CrudContext } from "./crudContext";
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
-    const crudContext = useContext(CrudContext)
-    const { fetchFromGuestCart } = crudContext
+  const crudContext = useContext(CrudContext);
+  const { fetchFromGuestCart } = crudContext;
 
-    const [user, setUser] = useState(null);
-    const [userIP, setUsetIp] = useState("");
-    const [isUserAdmin, setIsUserAdmin] = useState(false)
+  const [user, setUser] = useState(null);
+  const [userIP, setUsetIp] = useState("");
+  const [isUserAdmin, setIsUserAdmin] = useState(false);
 
+  const getJSON = () => {
+    const url =
+      "https://ipfind.co/?ip=79.67.160.179&auth=45cd7774-c99d-4aa8-95f3-426d14eabc46";
+    const proxyUrl = `https://agile-island-79839.herokuapp.com/`;
+    fetch(proxyUrl + url)
+      .then((res) => res.json())
+      .then((res) => {
+        setUsetIp(res.ip_address);
+      })
+      .then(fetchFromGuestCart)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const getJSON = () => {
-        const url = `https://api.astroip.co/2.99.115.173?api_key=a45dc99e-f914-4961-8009-54fca96d8819`
-        const proxyUrl = `https://agile-island-79839.herokuapp.com/`
-        fetch(proxyUrl + url)
-            .then((res) => res.json())
-            .then((res) => {
-                setUsetIp(res.requester_ip);
-            })
-            .then(fetchFromGuestCart)
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  const fetchFromUserAdmin = () => {
+    firestore
+      .collection("users")
+      .doc(user.uid)
+      .collection("admin")
+      .get()
+      .then((querySnapshot) => {
+        const currentData = querySnapshot.docs.map((doc) => doc.data());
+        setIsUserAdmin(currentData[0].admin);
+      })
+      .catch();
+  };
 
-    const fetchFromUserAdmin = () => {
-        firestore
-            .collection("users")
-            .doc(user.uid)
-            .collection("admin")
-            .get()
-            .then((querySnapshot) => {
-                const currentData = querySnapshot.docs.map((doc) => doc.data());
-                setIsUserAdmin(currentData[0].admin);
-            })
-            .catch();
-    };
+  const signIn = () => {
+    firebase.auth().signInWithRedirect(provider);
+  };
 
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(null);
+      })
+      .then(navigate("/"))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const signIn = () => {
-        firebase.auth().signInWithRedirect(provider)
-    };
-
-    const signOut = () => {
-        firebase
-            .auth()
-            .signOut()
-            .then(() => {
-                setUser(null);
-            }).then(
-                navigate('/')
-            )
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    const getUser = () => {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-                navigate("/products")
-            } else {
-                setUser(null);
-            }
-        });
-    };
-
-
-    useEffect(() => {
-        getUser();
+  const getUser = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        navigate("/products");
+      } else {
+        setUser(null);
+      }
     });
+  };
 
-    useEffect(() => {
-        getJSON();
-    }, [userIP]);
+  useEffect(() => {
+    getUser();
+  });
 
-    useEffect(() => {
-        if (user) {
-            fetchFromUserAdmin()
-        }
-    }, [user])
+  useEffect(() => {
+    getJSON();
+  }, [userIP]);
 
+  useEffect(() => {
+    if (user) {
+      fetchFromUserAdmin();
+    }
+  }, [user]);
 
-
-
-    return (
-        <UserContext.Provider value={{ user, signIn, signOut, userIP, isUserAdmin }}>
-            {props.children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider
+      value={{ user, signIn, signOut, userIP, isUserAdmin }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
 };
