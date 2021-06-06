4f23cff31ca890c602154483778101c57414f5f2
Ezio Intravaia <ezio.intravaia@hotmail.com>
2021-06-06 22:22:23 +0100

    rebranded top section of homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,28 +1,32 @@
 import React, { useContext } from "react";
 import styles from "./HomePage.module.scss";
-import { UserContext } from "../../context/userContext"
+import { UserContext } from "../../context/userContext";
 import { Link, navigate } from "@reach/router";
+import logo from "../../assets/cannoli.jpg";
+import NavBar from "../NavBar";
 
 const HomePage = () => {
   const userContext = useContext(UserContext);
-  const { signIn, user } = userContext
+  const { signIn, user } = userContext;
   return (
-    <section className={styles.homePage}>
-      <article>
-        <h1>The Sicilian Shop</h1>
-        <p>Hello there! Welcome into the Sicilian Shop, in here you're gonna find awesome traditional sicilian food made with the freshest ingridients imported from Palermo, cooked from our sicilian chefs and ready to be delivered to you home!</p>
-        <button onClick={() => {
-          if (!user) {
-            signIn()
-          } else {
-            navigate("/products")
-          }
-        }}>LogIn with Google</button>
-        <Link to='/products'>
-          <button>Enter as a visitor</button>
-        </Link>
-      </article>
-    </section>
+    <>
+      <section className={styles.homePage}>
+        <div className={styles.heroImage}>
+          <img src={logo} alt="" />
+          <NavBar />
+        </div>
+
+        <h1>Best sicilian food</h1>
+        <p>
+          Hello there! Welcome into the Sicilian Shop, in here you're gonna find
+          awesome traditional sicilian food made with the freshest ingridients
+          imported from Palermo, cooked from our sicilian chefs and ready to be
+          delivered to you home!
+        </p>
+
+        <button onClick={() => navigate("/products")}>Shop Now</button>
+      </section>
+    </>
   );
 };
 
